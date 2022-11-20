25c6c26989ca1c0a39eb6af5bac6ab61967c8abf
Jeff Lewis <474547+halldorr@users.noreply.github.com>
2022-11-20 15:03:48 -0400

    Change to using statement for SQLite

--- Classes/Contact.cs
+++ Classes/Contact.cs
@@ -11,8 +11,8 @@ namespace Contacts.Classes
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
     }
 }
--- NewContactWindow.xaml.cs
+++ NewContactWindow.xaml.cs
@@ -39,11 +39,11 @@ namespace Contacts
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 
-            SQLiteConnection connection = new SQLiteConnection(databasePath);
-            connection.CreateTable<Contact>();
-            connection.Insert(contact);
-            connection.Close();
-
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            };
 
             Close();
         }
